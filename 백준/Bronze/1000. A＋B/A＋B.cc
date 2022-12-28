42858229bd1845b969477f9a88aba4ab98ab9655
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2022-12-28 11:28:41 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	/***** 백준 1000 *****/
+
+	int a, b;
+
+	cin >> a >> b;
+	cout << a + b;
+
+
+
+	return 0;
+}
