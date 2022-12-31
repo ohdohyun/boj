06d8d3857f08bb71f8f2f587ba7f35e83f9b7179
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2022-12-31 15:19:08 +0900

    [Silver II] Title: 이동하기, Time: 516 ms, Memory: 76980 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -28,26 +28,11 @@ public class Main {
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                dp[i][j] = map[i][j] + dp[i-1][j-1];
-                dp[i][j] = Math.max(dp[i][j], map[i][j] + dp[i-1][j]);
-                dp[i][j] = Math.max(dp[i][j], map[i][j] + dp[i][j-1]);
+                dp[i][j] = Math.max(map[i][j] + dp[i-1][j-1], Math.max(map[i][j] + dp[i-1][j], map[i][j] + dp[i][j-1]));
             }
         }
-
-        // print map
-//        for(int i=0;i<=n;i++){
-//            for(int j=0;j<=m;j++){
-//                System.out.print(dp[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-
+        
         System.out.println(dp[n][m]);
-
-
-
-
-
 
         br.close();
     }
