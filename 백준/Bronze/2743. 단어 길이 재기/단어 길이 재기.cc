c68fb8a79a212df0eea42a2d18b6b7824d21423d
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:40:04 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// https://www.acmicpc.net/problem/1935
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	string str;
+
+	cin >> str;
+
+	cout << str.length();
+
+
+	return 0;
+}
