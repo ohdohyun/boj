bf630e9a230ae11b5f01039ea26a6d6b177a7526
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:39:48 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	int N;
+	cin >> N;
+	
+
+	for (int i = 1; i <= N; i++) {
+		cout << i << '\n';
+	}
+	
+	
+
+	return 0;
+}
+
+
