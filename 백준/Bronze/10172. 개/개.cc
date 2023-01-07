29df0cc04bf25375c3e0ea0dc983b85ef37a97ab
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:49:49 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	/***** 백준 10172 *****/
+
+	cout << "|\\_/|\n";
+	cout << "|q p|   /}\n";
+	cout << "( 0 )\"\"\"\\\n";
+	cout << "|\"^\"`    |\n";
+	cout << "||_/=\\\\__|\n";
+
+
+
+	return 0;
+}
