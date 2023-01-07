a388d7874a851050d4a50b38c2065e5d6a0a537c
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 15:02:30 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 256 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	int T;
+	cin >> T;
+	int A, B;
+
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		cout << A + B << '\n';
+	}
+	
+	
+
+	return 0;
+}
+
+
