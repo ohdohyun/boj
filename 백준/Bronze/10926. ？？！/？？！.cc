be0a53d0c3f1704db16cd3a391f2cc79707183e7
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:53:17 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	string n;
+	cin >> n;
+	
+	cout << n << "??!\n";
+
+	return 0;
+}
