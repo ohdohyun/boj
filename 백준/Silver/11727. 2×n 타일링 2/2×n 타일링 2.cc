a11a7d28ecd335bc0bc5bb37e1b4e6410237a864
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:59:02 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[1001];
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	dp[1] = 1;
+	dp[2] = 3;
+	
+	int n;
+	cin >> n;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+	}
+
+	cout << dp[n] << '\n';
+	
+
+	return 0;
+}
