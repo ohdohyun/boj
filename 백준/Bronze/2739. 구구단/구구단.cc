5ab5d295e4b5ea4e60757f2f5eee467877294145
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:39:39 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	int input;
+
+	cin >> input;
+
+	for (int i = 1; i < 10; i++) {
+
+		cout << input << " * " << i << " = " << input * i << '\n';
+	}
+
+	
+	
+	
+
+	return 0;
+}
+
+
