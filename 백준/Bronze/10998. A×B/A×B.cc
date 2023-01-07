613bed911bddad6868f64dfa8685fc4876152b25
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:54:05 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	/***** 백준 10998 *****/
+
+	int a, b;
+
+	cin >> a >> b;
+	cout << a * b;
+
+
+
+	return 0;
+}
