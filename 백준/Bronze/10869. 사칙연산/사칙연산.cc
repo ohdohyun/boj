e1d5299cd1b0bfa288838100fddee437959ca3d6
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:52:45 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	/***** 백준 10869 *****/
+
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << a + b << '\n' << a - b << '\n' << a * b << '\n' << a / b << '\n' << a % b << '\n';
+
+
+
+	return 0;
+}
