4b711d4631db6ddc0f35c92656d143229d664401
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:45:48 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	/***** 백준 6749 *****/
+
+	int Y, M;
+
+
+	cin >> Y >> M;
+
+	cout << M * 2 - Y;
+
+
+
+	return 0;
+}
