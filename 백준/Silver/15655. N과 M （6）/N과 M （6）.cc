74cdf6c8e0c1bf03fc97f9c5d35ec8342b56282a
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 15:03:35 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+// https://www.acmicpc.net/problem/15655
+
+int n, m;
+int result[10];
+int isused[10];
+vector<int> numbers;
+void rec(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << numbers[result[i] - 1] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (isused[i] == 0) {
+			if (k == 0 || result[k - 1] < i) {
+				isused[i] = 1;
+				result[k] = i;
+				rec(k + 1);
+				isused[i] = 0;
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int input;
+		cin >> input;
+		numbers.push_back(input);
+	}
+	sort(numbers.begin(), numbers.end());
+	rec(0);
+
+	return 0;
+}
