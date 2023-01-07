f9c4dd56d65718584925c06307db22df7fba8073
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:48:45 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+  int n; cin>>n;
+  if(n%2 == 1){cout << "SK\n";}
+  else {cout << "CY\n";}
+
+	return 0;
+}
