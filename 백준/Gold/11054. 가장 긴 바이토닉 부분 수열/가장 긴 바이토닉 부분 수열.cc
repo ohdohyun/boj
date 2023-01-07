fa6365b58afba48670887a3d73e228ee573f4342
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:55:34 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 1001
+
+int input[MAX];
+int dp[MAX];
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int N; cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> input[i];
+	}
+
+	int result = 0;
+
+	for (int i = 1; i <= N; i++) {
+		dp[i] = 1;
+		for (int j = 1; j < i; j++) {
+			if (input[j] < input[i]) {
+				dp[i] = max(dp[j] + 1, dp[i]);
+			}
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j < i; j++) {
+			if (input[i] < input[j]) {
+				dp[i] = max(dp[j] + 1, dp[i]);
+			}
+		}
+
+		result = max(dp[i], result);
+	}
+
+	cout << result << '\n';
+
+
+	return 0;
+}
