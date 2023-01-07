91ad64581b405edbe5a8660d0272a86cf182a0c0
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 15:02:14 +0900

    [Gold V] Title: 용액 합성하기, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+// https://www.acmicpc.net/problem/2467
+
+int arr[100000];
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int n; cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	//sort(arr, arr + n);
+	int st = 0, ed = n - 1;
+	int ans1 = 0, ans2 = 0, ansSum = 2000000001;
+	while (st < ed) {
+		int sum = arr[st] + arr[ed];
+
+		//cout << arr[st] << ", " << arr[ed] << " //// st, ed = " << st << ", " << ed << '\n';
+
+		if (ansSum > abs(sum)) {
+			ansSum = abs(sum);
+			ans1 = arr[st]; ans2 = arr[ed];
+			if (sum == 0) { break; }
+		}
+
+		if (sum < 0) {
+			st++;
+		}
+		else if (sum > 0) { ed--; }
+	}
+
+	cout << ans1 + ans2 << '\n';
+
+
+
+
+	return 0;
+}
