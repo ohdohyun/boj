77a07f852d9a327e4b51c6c323fb248b3d008014
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:36:51 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+// https://www.acmicpc.net/problem/2558
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	long long a, b;
+	cin >> a >> b;
+	cout << a + b;
+	
+	return 0;
+}
