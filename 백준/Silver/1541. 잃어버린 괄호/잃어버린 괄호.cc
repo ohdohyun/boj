69eaaef82e3d904802c39d8a0b85104d7fc7dc2b
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:17:05 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    string input;
+    cin >> input;
+
+    int result = 0;
+    string num;
+    bool isMinus = false;
+
+    for (int i = 0; i <= input.size(); i++) {
+
+        if (input[i] == '-' || input[i] == '+' || i == input.size()) {
+            if (isMinus) {
+                result -= stoi(num);
+                num = "";
+            }
+            else {
+                result += stoi(num);
+                num = "";
+            }
+        }
+        else {
+            num += input[i];
+        }
+
+        if (input[i] == '-') {
+            isMinus = true;
+        }
+    }
+
+    cout << result;
+
+
+
+
+	return 0;
+}
