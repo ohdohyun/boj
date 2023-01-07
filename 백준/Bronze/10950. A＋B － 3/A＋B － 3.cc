8ac900ed85b842b9e7302ce40843d022ff3f82e6
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 14:53:26 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	int T;
+	int A, B;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		cout << A + B << '\n';
+
+	}
+
+	
+	
+	
+
+	return 0;
+}
+
+
