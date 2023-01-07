c16ad377dc337053d9931f030fcdb8f8a9634b2d
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-07 15:03:51 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+// https://www.acmicpc.net/problem/15657
+
+int n, m;
+int result[10];
+int isused[10];
+vector<int> numbers;
+void rec(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << numbers[result[i] - 1] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!isused[i]) {
+			if (k==0 || result[k-1] <= i) {				// 조건 채우기 N과 M(8)
+				//isused[i] = 1;
+				result[k] = i;
+				rec(k + 1);
+				//isused[i] = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int input;
+		cin >> input;
+		numbers.push_back(input);
+	}
+	sort(numbers.begin(), numbers.end());
+	rec(0);
+
+	return 0;
+}
