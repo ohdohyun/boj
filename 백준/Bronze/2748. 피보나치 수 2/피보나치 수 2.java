9063be10a124bdca15e3294019b761ba4ad677bc
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-31 10:11:48 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 132 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[n + 1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        if (n < 2) {
+            System.out.println(arr[n]);
+            return;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.println(arr[n]);
+
+
+    }
+}
+
