f4f1b1cb1a90abdebac7c7249b9f19a4c97f3751
ohdohyun <68079179+ohdohyun@users.noreply.github.com>
2023-01-31 14:04:20 +0900

    [Gold V] Title: 내려가기, Time: 404 ms, Memory: 45072 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -5,57 +5,68 @@ import java.util.*;
 public class Main {
 
     static StringTokenizer st;
-    static int map[][];
     static int maxDp[][];
     static int minDp[][];
+    static int input[];
+
+    /*static void printMap() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(maxDp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }*/
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        map = new int[n][3];
-        maxDp = new int[n][3];
-        minDp = new int[n][3];
+        maxDp = new int[2][3];
+        minDp = new int[2][3];
+        input = new int[3];
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        for (int i = 0; i < 3; i++) {
-            maxDp[0][i] = map[0][i];
-            minDp[0][i] = map[0][i];
+        st = new StringTokenizer(br.readLine());
+        for (int j = 0; j < 3; j++) {
+            input[j] = minDp[1][j] = maxDp[1][j] = minDp[0][j] = maxDp[0][j] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < 3; j++) {
+            st = new StringTokenizer(br.readLine());
 
+            for (int j = 0; j < 3; j++) {
+                maxDp[0][j] = maxDp[1][j];
+                minDp[0][j] = minDp[1][j];
+            }
+            for (int j = 0; j < 3; j++) {
+                input[j] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int j = 0; j < 3; j++) {
                 switch (j) {
                     case 0:
-                        maxDp[i][j] = Math.max(maxDp[i - 1][0], maxDp[i - 1][1]);
-                        maxDp[i][j] += map[i][j];
-                        minDp[i][j] = Math.min(minDp[i - 1][0], minDp[i - 1][1]);
-                        minDp[i][j] += map[i][j];
+                        maxDp[1][0] = Math.max(maxDp[0][0], maxDp[0][1]) + input[0];
+                        minDp[1][0] = Math.min(minDp[0][0], minDp[0][1]) + input[0];
                         break;
                     case 1:
-                        maxDp[i][j] = Math.max(maxDp[i - 1][0], Math.max(maxDp[i - 1][1], maxDp[i - 1][2]));
-                        maxDp[i][j] += map[i][j];
-                        minDp[i][j] = Math.min(minDp[i - 1][0], Math.min(minDp[i - 1][1], minDp[i - 1][2]));
-                        minDp[i][j] += map[i][j];
+                        maxDp[1][1] = Math.max(maxDp[0][0], Math.max(maxDp[0][1], maxDp[0][2])) + input[1];
+                        minDp[1][1] = Math.min(minDp[0][0], Math.min(minDp[0][1], minDp[0][2])) + input[1];
                         break;
                     case 2:
-                        maxDp[i][j] = Math.max(maxDp[i - 1][1], maxDp[i - 1][2]);
-                        maxDp[i][j] += map[i][j];
-                        minDp[i][j] = Math.min(minDp[i - 1][1], minDp[i - 1][2]);
-                        minDp[i][j] += map[i][j];
+                        maxDp[1][2] = Math.max(maxDp[0][1], maxDp[0][2]) + input[2];
+                        minDp[1][2] = Math.min(minDp[0][1], minDp[0][2]) + input[2];
                         break;
                 }
             }
+
+            /*System.out.println("input[" + 0 + "] = " + input[0] + ", input[" + 1 + "] = " + input[1] + ", input[" + 2 + "] = " + input[2]);
+            printMap();*/
         }
 
-        System.out.print(Math.max(maxDp[n - 1][0], Math.max(maxDp[n - 1][1], maxDp[n - 1][2])) + " ");
-        System.out.println(Math.min(minDp[n - 1][0], Math.min(minDp[n - 1][1], minDp[n - 1][2])));
+        System.out.print(Math.max(maxDp[1][0], Math.max(maxDp[1][1], maxDp[1][2])) + " ");
+        System.out.println(Math.min(minDp[1][0], Math.min(minDp[1][1], minDp[1][2])));
 
 
         br.close();
